c103767a5b6f2a8a8a2f31197056630bea24ce56
jzegarrag <jzegarrag@unjbg.edu.pe>
2024-07-15 01:14:18 -0500

    añadir la segunda parte del codigo

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -14,7 +14,7 @@ int main(){
 	int i;
 	float sueldo;
 	
-	cout<<"ingrese la cantidad de empleados: cin>>e;
+	cout<<"ingrese la cantidad de empleados: "; cin>>e;
 	
 	empleado personas[e];
 	for(int i = 0 ;i < e ; i++)
@@ -26,11 +26,31 @@ int main(){
 		getline(cin,personas[i].nombre);
 			cout<<"sueldo del empleado: ";
 		cin>>sueldo;
-			cout<<"sexo del empleado masculino o femenino: 
+			cout<<"sexo del empleado masculino o femenino: ";
 		cin.ignore();
 		getline(cin,personas[i].sexo);
 	}
-	
+	empleado* mayorSalario = &personas[0];
+    empleado* menorSalario = &personas[0];
+    
+     for (int i = 1; i < e; ++i) {
+        if (personas[i].sueldo > mayorSalario->sueldo) {
+            mayorSalario = &personas[i];
+        }
+        if (personas[i].sueldo < menorSalario->sueldo) {
+            menorSalario = &personas[i];
+        }
+    }
+    
+    cout << "\nEmpleado con mayor salario:\n";
+    cout << "Nombre: " << mayorSalario->nombre << "\n";
+    cout << "Sexo: " << mayorSalario->sexo << "\n";
+    cout << "Sueldo: " << mayorSalario->sueldo << "\n";
+
+    cout << "\nEmpleado con menor salario:\n";
+    cout << "Nombre: " << menorSalario->nombre << "\n";
+    cout << "Sexo: " << menorSalario->sexo << "\n";
+    cout << "Sueldo: " << menorSalario->sueldo << "\n";
 	return 0;
 	
 }
